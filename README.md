2536f690afbe301b8d1a0f55c31d9440749ed3d8
Nicolas VANHAEZEBROUCK <nvanhaezebrouck@CASTELIS.COM>
2017-10-20 10:50:04 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # config
-Config PHP classe
+Une classe de gestion de fichier de configuration pour mon framework
+
+## Utilisation
+
+Initialisation au plus tôt dans le code
+```php
+// initialise le singleton de configuration
+\VekaServer\Config\Config::getInstance(__DIR__.'/config/config.php');
+```
+
+Recuperation d'une valeur
+```php
+\VekaServer\Config\Config::getSetting('db_pass');
+```
+
+## Exemple de fichier de config
+```php
+<?php
+
+return array(
+
+    // Environnement
+    "ENV"   => 'DEV', // DEV / PROD
+
+    // BDD
+    "db_user" => 'root',
+    "db_pass" => 'root',
+    "db_host" => 'localhost',
+    "db_name" => 'mabase'
+
+);
+```
+
+
---
+++ src/Config.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nvanhaezebrouck
+ * Date: 19/10/2017
+ * Time: 20:52
+ */
+
+namespace VekaServer\Config;
+
+class Config
+{
+
+    private $settings = [];
+
+    private static $_instance = null;
+
+    /**
+     * Retourne une instance singleton du fichier de config
+     * @param null $path_config_file Chemin vers le fichier de config
+     * @return null|Config
+     */
+    public static function getInstance($path_config_file = null){
+
+        if(self::$_instance instanceof self)
+            return self::$_instance;
+
+        return new self($path_config_file);
+
+    }
+
+    /**
+     * Config constructor.
+     * @param string $path_config_file Chemin vers le fichier de configuration
+     */
+    public function __construct($path_config_file)
+    {
+        $this->settings = require_once($path_config_file);
+    }
+
+    /**
+     * Get setting by key
+     *
+     * @param $key
+     * @return mixed
+     * @throws \Exception Key not found on config file
+     */
+    public function getSetting($key){
+
+        if(isset($this->settings[$key]))
+            return $this->settings[$key];
+
+        throw new \Exception('Key "'.$key.'" not found on config file.');
+    }
+
+    /**
+     * Get all settings
+     *
+     * @return mixed
+     */
+    public function getSettings(){
+        return $this->settings;
+    }
+
+}
